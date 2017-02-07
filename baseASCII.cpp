47864c97094cebac13e863325127775c4e6d35e5
Jakub Trnka <jakub.trnka@gmx.com>
2017-02-07 22:34:58 +0100

    ascii newline fix

--- baseASCII.cpp
+++ baseASCII.cpp
@@ -17,6 +17,8 @@ namespace baseASCII
                         unsigned char buff[1024];
                         ist.read( (char * ) buff, 1024 );
                         int imax = ist.gcount();
+                        if ( ( buff[ imax - 1 ] == '\n' ) && ist.eof() )
+                                imax -- ;
                         for ( int i=0; i<imax; i++ )
                         {
                                 out.push_back( (int) buff[ i ] );
